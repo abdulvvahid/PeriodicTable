81f241c03a568851116a77377ddaa07999919467
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-09-15 18:47:47 +0300

    Design dialog fragment

--- app/src/main/java/com/noor/periodictable/ui/ElementDialogFragment.kt
+++ app/src/main/java/com/noor/periodictable/ui/ElementDialogFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.noor.periodictable.databinding.FragmentElementDialogBinding
 
-class ElementDialogFragment : Fragment() {
+class ElementDialogFragment : DialogFragment() {
 
     private lateinit var binding: FragmentElementDialogBinding
 
