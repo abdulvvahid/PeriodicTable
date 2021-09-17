fa5dad78b967bf4c7674596a35aa0a2f79fcaa25
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-09-17 22:58:20 +0300

    Add item animator

--- app/src/main/java/com/noor/periodictable/adapter/viewholder/ElementHolder.kt
+++ app/src/main/java/com/noor/periodictable/adapter/viewholder/ElementHolder.kt
@@ -1,13 +1,15 @@
 package com.noor.periodictable.adapter.viewholder
 
+import android.animation.Animator
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.noor.periodictable.databinding.ItemRvMainActivityBinding
 import com.noor.periodictable.model.Element
+import jp.wasabeef.recyclerview.animators.holder.AnimateViewHolder
 
 class ElementHolder(
     private val binding: ItemRvMainActivityBinding
-) : RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root), AnimateViewHolder {
 
     var itemClickListener: ((view: View, element: Element, position: Int, code: Int) -> Unit)? = null
 
@@ -17,6 +19,39 @@ class ElementHolder(
         binding.llItem.setOnClickListener {
             itemClickListener?.invoke(it, element, adapterPosition, 0)
         }
+    }
+
+    override fun animateAddImpl(
+        holder: RecyclerView.ViewHolder,
+        listener: Animator.AnimatorListener
+    ) {
+        itemView.animate().apply {
+            translationY(0f)
+            alpha(1f)
+            duration = 300
+            setListener(listener)
+        }.start()
+
+    }
+
+    override fun animateRemoveImpl(
+        holder: RecyclerView.ViewHolder,
+        listener: Animator.AnimatorListener
+    ) {
+        itemView.animate().apply {
+            translationY(-itemView.height * 0.3f)
+            alpha(0f)
+            duration = 300
+            setListener(listener)
+        }.start()
+    }
+
+    override fun preAnimateAddImpl(holder: RecyclerView.ViewHolder) {
+    }
+
+    override fun preAnimateRemoveImpl(holder: RecyclerView.ViewHolder) {
+        itemView.setTranslationY(-itemView.height * 0.3f)
+        itemView.setAlpha(0f)
     }
 
 }
--- app/src/main/java/com/noor/periodictable/ui/HomeFragment.kt
+++ app/src/main/java/com/noor/periodictable/ui/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,8 @@ import com.noor.periodictable.adapter.PeriodicTableAdapter
 import com.noor.periodictable.databinding.FragmentHomeBinding
 import com.noor.periodictable.util.ElementList.createElementList
 import com.noor.periodictable.util.ItemDecoration
+import jp.wasabeef.recyclerview.animators.FadeInUpAnimator
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 
 class HomeFragment : Fragment() {
 
@@ -46,6 +49,13 @@ class HomeFragment : Fragment() {
                 resources.getDimensionPixelSize(R.dimen.element_spacing)
             )
         )
+        binding.rvTable.itemAnimator = FadeInUpAnimator()
+        binding.rvTable.itemAnimator?.apply {
+            addDuration = 1000
+            removeDuration = 100
+            moveDuration = 1000
+            changeDuration = 100
+        }
         tableAdapter.itemClickListener = { _, element, _, _ ->
             if (element.abbreviation != null) {
                 val dialog = ElementDialogFragment.newInstance(element)
